24052c79e6833b35e757abfa2432f01e2ace2095
FIRST_NAME LAST_NAME <savenkomax05@gmail.com>
2024-05-21 18:41:03 +0300

    all

--- lesson9/script.js
+++ lesson9/script.js
@@ -1,17 +1,21 @@
 const cars = [];
 const elem = document.getElementById('app');
-function getData(){
+const up = document.getElementById('up')
+function getData() {
     const brand = document.getElementById('brand');
     const model = document.getElementById('model');
     const imgUrl = document.getElementById('imgUrl');
     const descr = document.getElementById('descr');
+    const price = document.getElementById('price');
 
 
     const items = {
+        'id': (Math.random()).toString().split('.')[1],
         'brand': brand.value,
         'model': model.value,
-        'imgUrl':imgUrl.value,
-        'descr':descr.value,
+        'imgUrl': imgUrl.value,
+        'price': price.value,
+        'descr': descr.value,
 
     }
 
@@ -20,15 +24,21 @@ function getData(){
 
 }
 
-function banner(){
+function banner() {
     const card = document.querySelectorAll('.card');
-    const app = document.querySelector('app');
-    if (card.length !== 0){
+    const app = document.querySelector('#app');
+    if (card.length > 0) {
         app.innerHTML = '';
     }
-    
-    for(let i = 0; i < cars.length; i++){
-        createCard(cars[i].imgUrl, cars[i].brand, cars[i].model, cars[i].descr)
+
+    for (let i = 0; i < cars.length; i++) {
+        createCard(
+            cars[i].imgUrl,
+            cars[i].brand,
+            cars[i].model,
+            cars[i].price,
+            cars[i].descr,
+        )
     }
 
     // let pattern = '';
@@ -43,18 +53,25 @@ function banner(){
     //                 <a href="#" class="btn btn-primary">Go somewhere</a>
     //             </div>
     //         </div>
-        
+
     //     `
     // }
-    
+
     // elem.innerHTML = pattern
 
 }
+const deleteCard = (e) => {
+    const { id } = e.currentTarget.dataset;
+    const filteredCard = cars.filter(car => car.id !== id);
+    cars = [...filteredCard];
+    banner();
+}
+
 
 const createCard = (...data) => {
-    const [ imgUrl, brand, model, descr ] = data;
+    const [imgUrl, brand, model, price, descr] = data;
 
-``
+    ``
 
     const divCard = document.createElement('div');
     div.setAttribute('class', 'card');
@@ -71,24 +88,30 @@ const createCard = (...data) => {
     const h5Text = document.createTextNode(`${brand} ${model}`);
     h5.appendChild(h5Text)
 
+    const priceP = document.createElement('p');
+    priceP.setAttribute('class', 'card-text');
+    const pricePText = document.createTextNode(`${price}`);
+    priceP.appendChild(pricePText)
+
     const p = document.createElement('p');
     p.setAttribute('class', 'card-text');
     const pText = document.createTextNode(`${descr}`);
     p.appendChild(pText)
 
     const a = document.createElement('a');
-    const aTeext = document.createTextNode(`${imgUrl}`)
+    const aText = document.createTextNode(`${imgUrl}`)
     a.setAttribute('class', 'btn btn-primary');
 
     divBody.appendChild(img);
     divBody.appendChild(h5);
     divBody.appendChild(p);
+    divBody.appendChild(priceP)
 
     divCard.appendChild(divBody);
     elem.appendChild(divCard)
 
 
-    
+
 }
 
 createCard('mers.webp', 'mersedes', 'e', 'lorem')
